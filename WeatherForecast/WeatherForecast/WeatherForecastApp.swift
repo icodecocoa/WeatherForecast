c4a4e628fd20f2ed0ba00be7ece37f4e2b641cec
drashtiZeo <icodecocoa@gmail.com>
2025-10-23 17:02:34 +0400

    Initial Project Creation

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Drashti Lakhani on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
