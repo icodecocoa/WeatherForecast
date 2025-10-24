fdeeb06365aeb3a54662d88b4b3ae98204c0c325
drashtiZeo <icodecocoa@gmail.com>
2025-10-24 15:29:33 +0400

    Create Weather List View and added Background Image according to the weather

---
+++ WeatherForecast/WeatherForecast/Sources/Views/WeatherListView.swift
@@ -0,0 +1,58 @@
+//
+//  WeatherListView.swift
+//  WeatherForecast
+//
+//  Created by Drashti Lakhani on 10/24/25.
+//
+
+import SwiftUI
+
+struct WeatherListView: View {
+    @StateObject var viewModel: WeatherListViewModel
+    
+    var body: some View {
+        Group {
+            switch viewModel.state {
+            case .idle:
+                Button("Tap to load weather") {
+                    Task {
+                        await viewModel.fecthWeather()
+                    }
+                }
+            case .loading:
+                ProgressView()
+            case .loaded(let days):
+                ZStack {
+                    BackgroundView(condition: days.first?.weather.first?.main ?? .clear)
+                }
+            case .failed(let error):
+                Text("Error: \(error)")
+            }
+        }
+    }
+}
+
+struct BackgroundView: View {
+    let condition: MainEnum
+    var body: some View {
+        // simple mapping
+        var name: String {
+            switch condition {
+            case .clear:
+                return "Sunny"
+            case .clouds:
+                return "Cloudy"
+            case .rain, .drizzle:
+                return "Rainy"
+            case .snow, .ash, .dust, .fog, .haze, .mist, .smoke, .tornado, .thunderstorm, .sand, .squall:
+                return "Forest"
+            }
+        }
+        GeometryReader { proxy in
+            Image(name)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+        }
+    }
+}
